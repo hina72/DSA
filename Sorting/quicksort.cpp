a08f0cd5bf866aec71f2f4bdfde81d425ce76281
Huỳnh Khánh Hưng <hug92t@gmail.com>
2026-01-23 21:42:15 +0700

    Create quicksort.cpp

---
+++ Sorting/quicksort.cpp
@@ -0,0 +1,39 @@
+// 21:41 23/01/2026
+#include <bits/stdc++.h>
+using namespace std;
+
+int prt(vector<int>& a, int l, int r){
+    int pivot = a[r];
+    int i = l - 1;
+    for(int j = l; j < r; j++){
+        if(a[j] < pivot){
+            i++;
+            swap(a[i], a[j]);
+        }
+    }
+    swap(a[i + 1], a[r]);
+    return i + 1;
+}
+void quicksort(vector<int>& a, int l, int r){
+    if(l < r){
+        int p = prt(a, l, r);
+        quicksort(a, l, p - 1);
+        quicksort(a, p + 1, r);
+    }
+}
+int main(){
+    vector<int> a;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int t;
+        cin >> t;
+        a.push_back(t);
+    }
+    quicksort(a, 0, n - 1);
+
+    for(int i : a){
+        cout << i << " ";
+    }
+    return 0;
+}
