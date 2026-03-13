f52b4401e198ba8af13e8320ddcccbfd8030c2de
Huỳnh Khánh Hưng <hug92t@gmail.com>
2026-03-13 10:31:23 +0700

    add co

--- Sorting/insertionSort.cpp
+++ Sorting/insertionSort.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 void insertionSort(int a[], int n){
     for(int i = 1; i < n; i++){
+        // key
         int k = a[i];
 
         int j = i - 1;
+        // insert a[i] to the position x that a[x-1] < a[i] (to the begin position if x-1 < 0)
         while(j >= 0 && a[j] > k){
             a[j + 1] = a[j];
             j--;
@@ -14,12 +16,14 @@ void insertionSort(int a[], int n){
     }
 }
 int main(){
+    // initializing and handle input
     int n;
     cin >> n;
     int a[n];
     for(int i = 0; i < n; i++){
         cin >> a[i];
     }
+    
     insertionSort(a, n);
     
     for(int i = 0; i < n; i++){
